7e06eacb72eb966dac2717a8a6020f20ddf8cc85
jeremie <jmartins@stagiaire-humanbooster.com>
2021-01-21 12:26:22 +0100

    ajout fonctionnalité

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-list';
+
+  public todoArray: Array<string> = [
+    'item 1',
+    'item 2',
+    'item 3',
+    'item 4',
+    'item 5',
+    'footbar',
+  ];
+// void = pour rien retourner
+  public addItem($event: KeyboardEvent): void {
+    if ($event.code === 'Enter') {
+      const $input: EventTarget|null = $event.target;
+      if ($input instanceof HTMLInputElement){
+        const str = $input.value.trim();
+        if (str !== ''){
+          this.todoArray.push(str);
+        }
+        $input.value = '';
+      }   
+    }
+  }
+  public removeItem(index: number): void {
+   
+    if (index > -1 && index < this.todoArray.length) {
+    this.todoArray.splice(index, 1);
+  }
+}
 }
